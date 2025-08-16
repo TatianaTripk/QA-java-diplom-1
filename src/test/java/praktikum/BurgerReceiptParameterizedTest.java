3182aab187ddeb1ae0876a5107a471796ca7a138
Tatiana Tripkovich <tripkovich.t@yandex.ru>
2025-08-16 14:08:56 +0300

    mocks added

--- src/test/java/praktikum/BurgerReceiptParameterizedTest.java
+++ src/test/java/praktikum/BurgerReceiptParameterizedTest.java
@@ -3,6 +3,7 @@ package praktikum;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -12,9 +13,13 @@ import static org.junit.Assert.assertEquals;
 @RunWith(Parameterized.class)
 public class BurgerReceiptParameterizedTest {
 
-    private final Bun bun;
-    private final Ingredient[] ingredients;
     private final String expectedReceipt;
+
+    @Mock
+    private final Ingredient[] ingredients;
+
+    @Mock
+    private Bun bun;
 
     public BurgerReceiptParameterizedTest(Bun bun, Ingredient[] ingredients, String expectedReceipt) {
         this.bun = bun;
@@ -25,7 +30,6 @@ public class BurgerReceiptParameterizedTest {
     @Parameterized.Parameters(name = "Тестовые данные: {0}, {1}, {2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-
                 {
                         new Bun("black bun", 100),
                         new Ingredient[]{},
@@ -43,7 +47,6 @@ public class BurgerReceiptParameterizedTest {
                                 "(==== white bun ====)\r\n\r\n" +
                                 "Price: 600,000000\r\n"
                 },
-
                 {
                         new Bun("red bun", 300),
                         new Ingredient[]{
@@ -57,7 +60,6 @@ public class BurgerReceiptParameterizedTest {
                                 "(==== red bun ====)\r\n\r\n" +
                                 "Price: 1100,000000\r\n"
                 },
-
                 {new Bun("black bun", 100),
                         new Ingredient[]{
                                 new Ingredient(IngredientType.SAUCE, "hot sauce", 100),
@@ -67,7 +69,6 @@ public class BurgerReceiptParameterizedTest {
                                 new Ingredient(IngredientType.FILLING, "dinosaur", 200),
                                 new Ingredient(IngredientType.FILLING, "sausage", 300)
                         },
-
                         "(==== black bun ====)\r\n" +
                                 "= sauce hot sauce =\r\n" +
                                 "= sauce sour cream =\r\n" +
@@ -78,12 +79,11 @@ public class BurgerReceiptParameterizedTest {
                                 "(==== black bun ====)\r\n\r\n" +
                                 "Price: 1400,000000\r\n"
                 }
-
         });
     }
 
     @Test
-    public void testGetReceiptParameterizedTest() {
+    public void getReceiptParameterizedTest() {
         Burger burger = new Burger();
         burger.setBuns(bun);
 
